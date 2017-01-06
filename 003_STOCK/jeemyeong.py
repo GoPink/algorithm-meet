4aa2bdabdaf4fa70fa0ab2331eeaade1e632d852
Jeemyeong Lee <jeemyeong@gmail.com>
2017-01-06 20:18:31 +0900

    stock modify

--- 003_STOCK/jeemyeong.py
+++ 003_STOCK/jeemyeong.py
@@ -1,19 +1,33 @@
-def sumToMax(inputList):
-	indexMax = inputList.index(max(inputList))
-	if indexMax == 0:
-		return 0
-	sumingToMax = 0
-	for i in range(indexMax):
-		sumingToMax += (inputList[indexMax] - inputList[i])
-	if len(inputList)-1 == indexMax:
-		return sumingToMax
-	sumingToMax += sumToMax(inputList[indexMax+1:])
-	return sumingToMax
+def stock(n, inputList):
+	start = False
+	currentMax = 0
+	sumToCurrent = 0
+	for i in range(n-1,-1,-1):
+		if not start:
+			start = True
+			currentMax = inputList[i]
+			continue
+		if currentMax >= inputList[i]:
+			sumToCurrent += currentMax-inputList[i]
+		else:
+			currentMax = inputList[i]
+	return sumToCurrent
 
-for case in range(int(input())):
-	N = int(input())
-	inputList = list(map(int, input().split()))
-	print(sumToMax(inputList))
+def run():
+	for _ in range(int(input())):
+		n = int(input())
+		inputList = list(map(int, input().split()))
+		print(stock(n, inputList))
 
+def testRun():
+	n = 3
+	inputList = [10,7,6]
+	print(stock(n,inputList)==0)
+	n = 3
+	inputList = [3,5,9]
+	print(stock(n,inputList)==10)
+	n = 5
+	inputList = [1,1,3,1,2]
+	print(stock(n,inputList)==5)
 
-
+run()
