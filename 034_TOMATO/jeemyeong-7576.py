517b9c3fa2c2fdeb64957bbaa25c66cdd132d5c8
Jeemyeong Lee <jeemyeong@gmail.com>
2017-01-11 17:29:45 +0900

    update with reference

--- 034_TOMATO/jeemyeong-7576.py
+++ 034_TOMATO/jeemyeong-7576.py
@@ -1,6 +1,8 @@
 # https://www.acmicpc.net/problem/7576
+import collections
+
 def tomatoBfs(m,n,tomatoMap):
-	queue = []
+	queue = collections.deque()
 	for i in range(n):
 		for j in range(m):
 			if tomatoMap[i][j] == 1:
@@ -11,10 +13,10 @@ def tomatoBfs(m,n,tomatoMap):
 	for _ in range(n):
 		visited.append([0]*m)
 	day = -1
-	while len(queue)>0:
+	while queue:
 		listInThisDay = []
 		for _ in range(len(queue)):
-			y,x = queue.pop(0)
+			y,x = queue.popleft()
 			visited[y][x] = 1
 			listInThisDay.append((y,x))
 		for y, x in listInThisDay:
@@ -36,6 +38,74 @@ def tomatoBfs(m,n,tomatoMap):
 		if 0 in i:
 			return -1
 	return day
+
+# UPDATE with reference
+def tomatoBfs2(m,n,tomatoMap):
+	direct = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+	queue = collections.deque()
+	for i in range(n):
+		for j in range(m):
+			if tomatoMap[i][j] == 1:
+				queue.append((i,j))
+	while queue:
+		y,x = queue.popleft()
+		for d in direct:
+			dy = y + d[0]
+			dx = x + d[1]
+			if dy<0 or dx<0 or dy>=n or dx>=m:
+				continue
+			if tomatoMap[dy][dx] == 0:
+				queue.append((dy,dx))
+				tomatoMap[dy][dx] = tomatoMap[y][x]+1
+	max_num = 0
+	for i in range(n):
+		for j in range(m):
+			if tomatoMap[i][j] == 0:
+				return -1
+			if max_num < tomatoMap[i][j]:
+				max_num = tomatoMap[i][j]
+	return max_num -1
+
+# BELOW is reference
+# 
+# import collections
+
+# def bfs():
+#     direct = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+#     q = collections.deque()
+#     for i in range(n):
+#         for j in range(m):
+#             if arr[i][j] == 1:
+#                 q.append([i, j])
+
+#     while q:
+#         y, x = q.popleft()
+#         for d in direct:
+#             dy = y + d[0]
+#             dx = x + d[1]
+#             if dy < 0 or dx < 0 or dy >= n or dx >= m:
+#                 continue
+#             if arr[dy][dx] == 0:
+#                 arr[dy][dx] = arr[y][x] + 1
+#                 q.append([dy, dx])
+
+#     max_num = 0
+#     for i in range(n):
+#         for j in range(m):
+#             if arr[i][j] == 0:
+#                 print(-1)
+#                 return
+#             if max_num < arr[i][j]:
+#                 max_num = arr[i][j]
+#     print(max_num - 1)
+
+
+# m, n = map(int, input().split())
+# arr = [[]] * n
+# for i in range(n):
+#     arr[i] = list(map(int, input().split()))
+
+# bfs()
 
 def pprint(tomatoMap):
 	for i in tomatoMap:
@@ -63,5 +133,5 @@ def run():
 		tomatoMap.append(list(map(int,input().split())))
 	print(tomatoBfs(m,n,tomatoMap))
 
-# run()
-testRun(testSource)
+run()
+# testRun(testSource)
